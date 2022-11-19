9ba3dadebda179b91ddfef60a51502979cf3b9db
ashikmahmud <ashikmamud187@gmail.com>
2022-11-20 00:17:24 +0600

    remove unnecessary

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -115,7 +115,6 @@ const sendVerificationEmail = async (email, token) => {
             <address>Rangpur Road, Gobindagonj, Gaibandha</address>
             <p>Mobile: +8801875474547</p>
             <p>Email: rent@house.com</p>
-            <p>${verificationUrl}</p>
         </div>
         `,
   };
